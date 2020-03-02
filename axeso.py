1dad46ce9ca816b736461f0db70a5ae4227cbae7
orlando-yerter <53948381+orlando-yerter@users.noreply.github.com>
2020-03-02 17:08:57 -0300

    Add files via upload

---
+++ axeso.py
@@ -0,0 +1,11 @@
+#import tkinter as tk
+from login import  Axeso
+Axeso()
+class Vent:
+    def __init__(self):
+      self.v1= tk.Tk()#constructor de la ventana , v1 
+              
+        
+      self.v1.mainloop() #bucle infinito 
+        
+        
